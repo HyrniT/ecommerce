c10f9de8e834ae532ad51830b0319b6a4d3181d9
Quang Hiển <ngqghien@gmail.com>
2024-01-22 14:57:18 +0700

    Add product model

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,15 +1,18 @@
 // controllers/home.controller.js
 
 const categoryModel = require('../models/category.model');
+const productModel = require('../models/product.model');
 
 module.exports = {
     getHome: async (req, res) => {
         const categories = await categoryModel.getAllCategories();
+        const products = await productModel.getAllProducts();
 
         res.render('index', { 
             title: 'OGANI | Home',
             name: req.user ? req.user.name : null,
             categories: categories,
+            products: products
         });
     }
 }
---
+++ models/product.model.js
@@ -0,0 +1,55 @@
+const db = require("../utils/database.js");
+
+class Product {
+    constructor(name, price, desc, img, quantity, categoryId) {
+        this.name = name;
+        this.price = price;
+        this.desc = desc;
+        this.img = img;
+        this.categoryId = categoryId;
+        this.quantity = quantity;
+    }
+
+    async saveProduct(name, desc, price, img, quantity, categoryId) {
+        try {
+            const result = await db.one('INSERT INTO "Products" ("name", "desc", "price", "img", "quantity", "category_id") VALUES ($1, $2, $3, $4, $5, $6) RETURNING "id"', [name, price, desc, img, quantity, categoryId]);
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getAllProducts() {
+        try {
+            const result = await db.any('SELECT * FROM "Products"');
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getProductById(id) {
+        try {
+            const result = await db.one('SELECT * FROM "Products" WHERE "id" = $1', [id]);
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async updateProduct(id, name, desc, price, img, quantity, categoryId) {
+        try {
+            if (img) {
+                const result = await db.result('UPDATE "Products" SET "name" = $1, "desc" = $2, "price" = $3, "img" = $4, "quantity" = $5, "category_id" = $6 WHERE "id" = $7', [name, desc, price, img, quantity, categoryId, id]);
+                return result;
+            } else {
+                const result = await db.result('UPDATE "Products" SET "name" = $1, "desc" = $2, "price" = $3, "quantity" = $4, "category_id" = $5 WHERE "id" = $6', [name, desc, price, quantity, categoryId, id]);
+                return result;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+module.exports = new Product();

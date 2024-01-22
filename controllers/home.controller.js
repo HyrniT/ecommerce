8af32e46304ced11aa8e9ddf59f20667f523c90f
Quang Hiển <ngqghien@gmail.com>
2024-01-22 11:42:14 +0700

    Add UI category

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,10 +1,15 @@
 // controllers/home.controller.js
 
+const categoryModel = require('../models/category.model');
+
 module.exports = {
-    getHome: (req, res) => {
+    getHome: async (req, res) => {
+        const categories = await categoryModel.getAllCategories();
+
         res.render('index', { 
             title: 'OGANI | Home',
             name: req.user ? req.user.name : null,
+            categories: categories,
         });
     }
 }

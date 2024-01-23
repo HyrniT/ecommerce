dee048bf972121f77419ddc5e9f606880e4c7ecb
20120077 <ngqghien@gmail.com>
2024-01-24 01:30:01 +0700

    Fix bugs: Category in details

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -62,12 +62,14 @@ module.exports = {
         const id = req.query.id;
         const product = await productModel.getProductById(id);
         const relatedProducts = await productModel.getRelatedProducts(product.category_id, id);
+        const categories = await categoryModel.getAllCategories();
 
         res.render('details', {
             shop: true,
             title: 'OGANI | Shop',
             name: req.user ? req.user.name : null,
             relatedProducts: relatedProducts,
+            categories: categories,
             id: id,
             nameProduct: product.name,
             price: product.price,
